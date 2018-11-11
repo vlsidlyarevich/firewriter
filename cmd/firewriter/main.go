ee37196b1fda0cc1a9dfd0d2b2491950a0da0a23
Uladzislau Sidliarevich <vlsidlyarevich@gmail.com>
2018-11-11 19:55:07 +0300

    Added port fetch from env variables. (Needed for Heroku).

--- cmd/firewriter/main.go
+++ cmd/firewriter/main.go
@@ -1,10 +1,17 @@
 package main
 
-import "github.com/vlsidlyarevich/firewriter/web/app"
+import (
+	"github.com/vlsidlyarevich/firewriter/web/app"
+	"os"
+)
 
 func main() {
 	var server = app.Server{}
+	port, ok := os.LookupEnv("PORT")
+	if !ok {
+		port = "8000"
+	}
 	server.Init()
 	server.InitHandlers()
-	server.Start("web/static", "8000")
+	server.Start("web/static", port)
 }
